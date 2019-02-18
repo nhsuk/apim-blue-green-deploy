4794b32eb15a4c1a545bdcf1c620f4eb322ed977
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2019-02-18 01:32:32 +0000

    :metal: Mock API Manager calls

--- test/integration/GetIndexNames.js
+++ test/integration/GetIndexNames.js
@@ -1,22 +1,29 @@
 const chai = require('chai');
+const nock = require('nock');
 const localSettings = require('../../local.settings.json');
 const getIndexNames = require('../../GetIndexNames/index');
 
 const expect = chai.expect;
 
 describe('GetIndexNames', () => {
+  afterEach('clean nock', () => {
+    nock.cleanAll();
+  });
+
   it('it should return names', async () => {
-    const context = {
-      log: () => {},
-    };
     process.env = localSettings.Values;
+    nock(`https://${process.env['apim-host-name']}`)
+      .get(/subscriptions/)
+      .times(1)
+      .query({ 'api-version': '2019-01-01' })
+      .reply(200, { properties: { serviceUrl: 'https://hostname/indexes/organisationlookup-2-0-b-dev/docs/' } });
+
+    const context = {
+      log: () => { },
+    };
     const response = await getIndexNames(context, 'service-search-organisations');
     expect(response).to.not.be.null;
-    const expectedResponses = [
-      'organisationlookup-2-0-a-dev',
-      'organisationlookup-2-0-b-dev'
-    ]
-    expect(expectedResponses).to.contain(response.idle);
-    expect(expectedResponses).to.contain(response.idle);
+    expect(response.idle).to.equal('organisationlookup-2-0-a-dev');
+    expect(response.active).to.equal('organisationlookup-2-0-b-dev');
   });
 });
